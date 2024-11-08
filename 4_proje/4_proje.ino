1bb09effa54bcb29413421d8d5987954db6659ec
Yaren <113533175+yarensari@users.noreply.github.com>
2024-11-09 02:22:43 +0300

    Proje 4 revize edildi.
    Arduino'nun dahili bir pull-up direncine sahip olduğu ve bunu kullanmak için INPUT_PULLUP modunun kullanıldığı fark edilmiştir. Kodda bir değişiklik yapılmamış sadece yorum satırındaki yanlış düzeltilmiştir.

--- 4_proje/4_proje.ino
+++ 4_proje/4_proje.ino
@@ -5,7 +5,7 @@ int butonDurum = 1;
 
 void setup() {
   Serial.begin(9600);           // Seri haberleşme 9600 baud rate ile başlatıldı.
-  /* 2. pine bağlanacak olan butona pull-up direnç bağlandığından dolayı 2. pinin konfigürasyonunda INPUT_PULLUP makrosu girilmiştir. */
+  /* 2. pine buton bağlanacaktır. Arduino'da yer alan dahili pull-up direncinin kullanılması amacıyla 2. pin INPUT_PULLUP modunda konfigüre edilmiştir. */
   pinMode(PIN_2, INPUT_PULLUP); // Dijital Giriş-Çıkış pinlerinden olan 2. pin INPUT_PULLUP olarak konfigüre edildi. Bu pinden butonun durumu okunacaktır.
   pinMode(PIN_12, OUTPUT);      // Dijital Giriş-Çıkış pinlerinden olan 12. pin OUTPUT olarak konfigüre edildi. Bu pinle led yakılıp söndürülecektir.
 
